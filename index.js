9b2d90a32328aaa82b46f2d145d3e785893ae0ad
Padmashree Jha <codewithpom@gmail.com>
2021-09-02 20:09:26 +0530

    Removed config.js because it was raising some exceptions

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-const config = require("./config")
+import express from "express";
+import twit from "twit";
+import dotenv from "dotenv";
+import path from 'path';
+const __dirname = path.resolve();
+dotenv.config();
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+const port = 5050;const T = new twit({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.token,
+    access_token_secret: process.env.token_secret,
+});
 
-config.app.get("/", (req, res) => {
+
+
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/frontend/index.html")
 })
 
-config.app.get("/profile/:screen_name", (req, res) => {
+app.get("/profile/:screen_name", (req, res) => {
     const options = {
         screen_name: req.params.screen_name,
     };
 
     console.log(options);
 
-    config.T.get("/users/show", options, function (err, data) {
+    T.get("/users/show", options, function (err, data) {
         console.log(data)
         res.removeHeader("x-powered-by")
         res.json(data)
     })
 })
 
-config.app.listen(config.port, "localhost", () => {
-    console.log(`config.app listening on port ${config.port}`)
+app.listen(port, "localhost", () => {
+    console.log(`app listening on port ${port}`)
 })
